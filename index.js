fb33d7a58e131579b834d6173d1bc7fd766cb40d
Yohann D <[yohanndurel@gmail.com]>
2023-03-01 17:12:48 +0100

    fix: allow cross origins

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 3000;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 require('./routes/product')(app);
 require('./routes/orders')(app);
