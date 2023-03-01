5b53f307e43baa92ab72e20868f90c7f155f08d4
Romain857 <romainmart.85@gmail.com>
2023-03-01 22:37:23 +0100

    testV2

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,7 @@
-var cors = require('cors')
-
-var corsOptions = {
-    origin: 'https://api-erp.vercel.app/',
-    optionsSuccessStatus: 200
-  }
-
-
 module.exports = (app) => {
     const product = require('../controllers/product');
 
-    app.get('/products',cors(corsOptions), product.findAll );
+    app.get('/products', product.findAll );
 
     app.get('/product/:idProduct', product.findOne);
 }
